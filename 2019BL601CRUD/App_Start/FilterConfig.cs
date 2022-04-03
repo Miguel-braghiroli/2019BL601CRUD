499fc527d7280085a47e0ece3bf1365ace93211b
Miguelbraghiroli <miguelangelbraghirolilemus@gmail.com>
2022-04-02 21:51:53 -0600

    crud

---
+++ 2019BL601CRUD/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _2019BL601CRUD
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 2019BL601CRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_2019BL601CRUD.MvcApplication" Language="C#" %>
